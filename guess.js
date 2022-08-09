e5a2b9a9eda8a17455bfb39eed51fbe1e3fee926
omerenma <godwin2341@gmail.com>
2022-08-09 11:32:37 +0100

    Binary search algorithym implemeted with guess game

---
+++ guess.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", () => {
+	let submit = document.querySelector(".submit");
+	let input_value = document.querySelector("#input_value");
+	let answer = document.querySelector(".answer");
+
+	const guesNumber = (arr, val) => {
+		let start = 0;
+		let end = arr.length - 1;
+
+		while (start <= end) {
+			let middle = Math.floor((end + start) / 2);
+			if (arr[middle] < val) {
+				start = middle + 1;
+			} else if (arr[middle] > val) {
+				end = middle - 1;
+			} else {
+				alert(`Your guess was correct at ${arr[middle]}`);
+				`Your answer is : ${answer.innerHTML = arr[middle]}`;
+				return;
+			}
+		}
+		return alert("Your guess was wrong!");
+	};
+
+	let data = [
+		2, 4, 6, 8, 9, 10, 20, 22, 30, 35, 36, 37, 38, 39, 40, 55, 56, 60, 67, 68,
+	];
+
+	submit.addEventListener("click", () => {
+		let num = parseInt(input_value.value);
+		guesNumber(data, num);
+		num = "";
+	});
+});
